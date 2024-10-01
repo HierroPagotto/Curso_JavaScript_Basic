a350d1b7b3ac2122538e22b14e6b443611338c78
HierroPagotto <hierro.reino@gmail.com>
2024-10-01 16:57:27 -0300

    update

---
+++ Modulo F/Aula16/funcao01.js
@@ -0,0 +1,12 @@
+function parimpar(n){
+    if (n%2 == 0){
+        return 'Par!'
+    }else{
+        return 'ímpar!'
+    }
+}
+
+let res = parimpar(4)
+console.log(res)
+//Outro jeito de fazer
+console.log(parimpar(15))
